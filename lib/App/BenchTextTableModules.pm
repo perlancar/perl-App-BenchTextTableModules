2de4cdc14866709cbf5154a5903dfbc89be39940
perlancar (@pc-office) <perlancar@gmail.com>
2015-06-24 13:43:04 +0700

    Release v0.01

--- lib/App/BenchTextTableModules.pm
+++ lib/App/BenchTextTableModules.pm
@@ -7,6 +7,25 @@ use 5.010001;
 use strict;
 use warnings;
 
+sub _make_table {
+    my ($cols, $rows) = @_;
+    my $res = [];
+    push @$res, [];
+    for (0..$cols-1) { $res->[0][$_] = "col" . ($_+1) }
+    for my $row (1..$rows) {
+        push @$res, [ map { "row$row.$_" } 1..$cols ];
+    }
+    $res;
+}
+
+our %tables = (
+    '0tiny(1x1)'     => _make_table( 1, 1),
+    '1small(3x5)'    => _make_table( 3, 5),
+    '2wide(30x5)'    => _make_table(30, 5),
+    '3long(3x300)'   => _make_table( 3, 300),
+    '4large(30x300)' => _make_table(30, 300),
+);
+
 our %contestants = (
     'Text::ANSITable' => sub {
         my ($table) = @_;
